05e03a6fa52a1559a02ca480b6094901527a0a19
Nilesh Kumar Singh <singhnilesh1998@gmail.com>
2022-03-28 17:15:37 +0530

    editing card

--- Script/script.js
+++ Script/script.js
@@ -21,20 +21,21 @@ function validation() {
     document.getElementById("lastt").innerHTML = " **Please fill Last Name";
     return false;
   }
-  if (!isNaN(lastname)) {
+  if (!isNaN(lastName)) {
     document.getElementById("lastt").innerHTML =
       " **Please fill character only";
     return false;
   }
-  // if ((email = "")) {
-  //   document.getElementById("emailCheck").innerHTML = " **Please Enter Email";
-  //   return false;
-  // }
-  // if (email.indexOf("@") <= 0) {
-  //   document.getElementById("emailCheck").innerHTML =
-  //     " **Please Enter Correct Email";
-  //   return false;
-  // }
+  if (email == "") {
+    document.getElementById("emailCheck").innerHTML = " **Please Enter Email";
+    return false;
+  }
+  if (email.indexOf("@") <= 0) {
+    document.getElementById("emailCheck").innerHTML =
+      " **Please Enter Correct Email";
+    return false;
+  }
+  return true;
 }
 
 //////////////////////////////
@@ -45,9 +46,13 @@ let btnModalAddEmployee = document.getElementById("bt_add_emp");
 let btnAddEmployee = document.getElementById("add_emp");
 let container = document.getElementById("targetRow");
 let Datacollection = [];
+let divFolderArray = [];
 const modal = document.querySelector(".modal");
+let detailsModal = document.querySelector("#contactModal1");
 const overlay = document.querySelector(".overlay");
+const overlay2 = document.querySelector(".overlay2");
 const btnCloseModal = document.querySelector(".close-modal");
+const btnCloseModal2 = document.querySelector(".close-modal2");
 const btnsOpenModal = document.querySelector("#add_emp");
 const openModal = function () {
   modal.style.display = "block";
@@ -58,6 +63,15 @@ const closeModal = function () {
   modal.style.display = "none";
   overlay.style.display = "none";
 };
+const openModal2 = function () {
+  detailsModal.style.display = "block";
+  overlay2.style.display = "block";
+};
+
+const closeModal2 = function () {
+  detailsModal.style.display = "none";
+  overlay2.style.display = "none";
+};
 
 //Add Employee Button
 btnAddEmployee.addEventListener("click", function () {
@@ -66,55 +80,70 @@ btnAddEmployee.addEventListener("click", function () {
 
   btnCloseModal.addEventListener("click", closeModal);
   overlay.addEventListener("click", closeModal);
+  document.querySelector(".Modaltitle").innerHTML = "Enter Employee Details";
+  document.getElementById("form-target").reset();
+  btnUpdateEmployee = document.querySelector("#btn_update");
 
+  btnUpdateEmployee.style.display = "none";
+  btnModalAddEmployee.style.display = "block";
   console.log(firstName + " in btnAddEmployee");
 });
 btnModalAddEmployee.addEventListener("click", function () {
-  templates = document.getElementsByTagName("template")[0];
-  let divFolderTemplate = templates.content.querySelector("#upper");
-  divFolder = document.importNode(divFolderTemplate, true);
-  console.log("hi i am clicked this is modal add button");
-  //first name
-  let name1 = document.getElementById("first-name").value;
-  //last name
-  let name2 = document.getElementById("last-name").value;
-  //job-title
-  let job = document.getElementById("job-titlee").value;
-  //department
-  let dept = document.getElementById("department").value;
-  //department
-  let Email = document.getElementById("email-check").value;
-  //department
-  let office = document.getElementById("office-input").value;
-  //department
-  let phoneNum = document.getElementById("phoneNum").value;
-  //department
-  let skype = document.getElementById("skype-email").value;
-  Datacollection.push({
-    name1,
-    name2,
-    job,
-    dept,
-    Email,
-    office,
-    phoneNum,
-    skype,
-  });
-  divFolder.querySelector("#tit0").innerHTML = name1 + " " + name2;
-  divFolder.querySelector("#tit1").innerHTML = job;
-  divFolder.querySelector("#tit2").innerHTML = dept;
+  if (validation()) {
+    templates = document.getElementsByTagName("template")[0];
+    let divFolderTemplate = templates.content.querySelector("#upper");
+    divFolder = document.importNode(divFolderTemplate, true);
+    console.log("hi i am clicked this is modal add button");
+    //first name
+    let name1 = document.getElementById("first-name").value;
+    //last name
+    let name2 = document.getElementById("last-name").value;
+    //job-title
+    let job = document.getElementById("job-titlee").value;
+    //department
+    let dept = document.getElementById("department").value;
+    //department
+    let Email = document.getElementById("email-check").value;
+    //department
+    let office = document.getElementById("office-input").value;
+    //department
+    let phoneNum = document.getElementById("phoneNum").value;
+    //department
+    let skype = document.getElementById("skype").value;
+    divFolder.querySelector("#tit0").innerHTML = name1 + " " + name2;
+    divFolder.querySelector("#tit1").innerHTML = job;
+    divFolder.querySelector("#tit2").innerHTML = dept;
+    let mid = Datacollection.length;
+    // let rid = Datacollection.length;
+    //divFolder.id = "" + rid
 
-  console.log("Hi " + name1 + " " + job + " " + dept);
+    Datacollection.push({
+      mid,
+      name1,
+      name2,
+      job,
+      dept,
+      Email,
+      office,
+      phoneNum,
+      skype,
+    });
+    storeInBowser();
 
-  console.log("success");
+    divFolderArray.push(divFolder);
+    console.log("check 123");
 
-  console.log(divFolder);
-  container.appendChild(divFolder);
-  document.getElementById("form-target").reset();
-  modal.style.display = "none";
-  overlay.style.display = "none";
+    console.log("Hi " + name1 + " " + job + " " + dept);
 
-  storeInBowser();
+    console.log("success");
+
+    console.log(divFolder);
+    container.appendChild(divFolder);
+    document.getElementById("form-target").reset();
+    modal.style.display = "none";
+    overlay.style.display = "none";
+  }
+  location.reload();
 });
 function storeInBowser() {
   let extractedfromArray = JSON.stringify(Datacollection);
@@ -131,12 +160,12 @@ function loadFromBrowser() {
   for (let i = 0; i < Datacollection.length; i++) {
     {
       createEmployeeOnLoad(
+        Datacollection[i].mid,
         Datacollection[i].name1,
         Datacollection[i].name2,
-        Datacollection[i].email,
+        Datacollection[i].Email,
         Datacollection[i].job,
         Datacollection[i].dept,
-        Datacollection[i].Email,
         Datacollection[i].office,
         Datacollection[i].phoneNum,
         Datacollection[i].skype
@@ -145,11 +174,12 @@ function loadFromBrowser() {
   }
 }
 function createEmployeeOnLoad(
+  mid,
   name1,
   name2,
+  Email,
   job,
   dept,
-  Email,
   office,
   phoneNum,
   skype
@@ -161,6 +191,25 @@ function createEmployeeOnLoad(
   divFolder.querySelector("#tit0").innerHTML = name1 + " " + name2;
   divFolder.querySelector("#tit1").innerHTML = job;
   divFolder.querySelector("#tit2").innerHTML = dept;
+
+  // divFolder = document.importNode(divFolderTemplate, true);
+  let view = divFolder.querySelector("[action=view]");
+  console.log("view created");
+  view.addEventListener("click", function () {
+    console.log("View is now clickable");
+    // console.log("Inside create details" + rname1 + " " + rname2);
+    document.querySelector("#mid").innerHTML = mid;
+    document.querySelector("#name-view").innerHTML = name1 + " " + name2;
+    document.querySelector("#email-view").innerHTML = Email;
+    document.querySelector("#job-view").innerHTML = job;
+    document.querySelector("#office-view").innerHTML = office;
+    document.querySelector("#dept-view").innerHTML = dept;
+    document.querySelector("#phno-view").innerHTML = phoneNum;
+    document.querySelector("#skype-view").innerHTML = skype;
+    openModal2();
+    btnCloseModal2.addEventListener("click", closeModal2);
+    overlay.addEventListener("click", closeModal2);
+  });
 
   container.appendChild(divFolder);
 }
@@ -276,9 +325,10 @@ let filterResources = [];
 
 let deptUl = document.querySelector(".list1");
 let depLi = deptUl.querySelectorAll(".list-items");
-
+let boldButton = "";
 for (let i = 0; i < depLi.length; i++) {
   depLi[i].addEventListener("click", function () {
+    boldButton = depLi[i];
     filterResources = [];
     depLi[i].style.fontWeight = "bold";
 
@@ -289,13 +339,12 @@ for (let i = 0; i < depLi.length; i++) {
         depLi[k].style.fontWeight = "normal";
       }
     }
-    let list = document.querySelector("#targetRow");
-    let totalDepartments = list.querySelectorAll("#department"); //total departments
-
+    let totalDepartments = container.querySelectorAll("#tit2"); //total departments
+    console.log(totalDepartments.length);
     for (var j = 0; j < totalDepartments.length; j++) {
       if (deptFilter == totalDepartments[j].innerHTML.toUpperCase()) {
         totalDepartments[j].parentElement.parentElement.style.display = "";
-        filterResources.push(totalDepartments[j].innerHTML);
+        filterResources.push(totalDepartments[j]);
         console.log(filterResources);
       } else {
         totalDepartments[j].parentElement.parentElement.style.display = "none";
@@ -303,3 +352,72 @@ for (let i = 0; i < depLi.length; i++) {
     }
   });
 }
+
+// $("#dd").on("change", function () {
+//   $("#last-name").val($(this).val());
+
+// updateFullName();
+// {
+//   console.log("triggered");
+//   $("#dd").val($("#firs-tname").val() + " " + $("#last-name").val());
+// }
+// // });
+// $("#first-name, #last-name").on("input", updateFullName);
+
+/* Edit details modal */
+
+let editDetails = document.querySelector(".edit_btn");
+editDetails.addEventListener("click", function () {
+  console.log("edit Details clicked");
+  openModal();
+  closeModal2();
+  btnCloseModal.addEventListener("click", closeModal);
+  overlay.addEventListener("click", closeModal);
+  //console.log(localStorage.getItem("rid"))
+  let index = document.querySelector("#mid").innerHTML;
+  console.log(index + " employee needed");
+  // modal.style.display = "block";
+  let employeeJson = localStorage.getItem("data");
+  let employees = JSON.parse(employeeJson);
+  //console.log(employees[0].name1)
+  btnUpdateEmployee = document.querySelector("#btn_update");
+
+  btnUpdateEmployee.style.display = "block";
+  btnModalAddEmployee.style.display = "none";
+
+  document.querySelector(".Modaltitle").innerHTML = "Update Employee Details";
+
+  document.getElementById("first-name").value = employees[index].name1;
+  document.getElementById("last-name").value = employees[index].name2;
+  document.getElementById("email-check").value = employees[index].Email;
+  document.getElementById("job-titlee").value = employees[index].job;
+  document.getElementById("office-input").value = employees[index].office;
+  document.getElementById("department").value = employees[index].dept;
+  document.getElementById("phoneNum").value = employees[index].phoneNum;
+  document.getElementById("skype").value = employees[index].skype;
+
+  btnUpdateEmployee.addEventListener("click", function () {
+    let name1 = document.getElementById("first-name").value;
+    let name2 = document.getElementById("last-name").value;
+    let email = document.getElementById("email-check").value;
+    let job = document.getElementById("job-titlee").value;
+    let office = document.getElementById("office-input").value;
+    let dept = document.getElementById("department").value;
+    let phno = document.getElementById("phoneNum").value;
+    let skypeid = document.getElementById("skype").value;
+
+    employees[index].name1 = name1;
+    employees[index].name2 = name2;
+    employees[index].Email = email;
+    employees[index].job = job;
+    employees[index].office = office;
+    employees[index].dept = dept;
+    employees[index].phoneNum = phno;
+    employees[index].skype = skypeid;
+
+    let njson = JSON.stringify(employees);
+    localStorage.setItem("data", njson);
+    closeModal2();
+    location.reload();
+  });
+});
